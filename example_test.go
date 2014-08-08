1f06f770ec9c63ab7d3a002b54c099b87f3d8cd4
Diego Guraieb <diego@pampanetwork.com>
2014-08-08 17:51:27 -0300

    examples

--- example_test.go
+++ example_test.go
@@ -2,11 +2,16 @@ package aranGO
 
 import "testing"
 import "log"
+import "encoding/json"
 
+type Sub struct{
+  SubAt  string `unique:"users"`
+}
 
 type DocTest struct {
   Document // arango Document to save id, key, rev
-  Name     string
+  Sub
+  Name     string  `json:"-,omitempty" unique:"users"`
   Age      int
   Likes    []string
 }
@@ -14,18 +19,52 @@ type DocTest struct {
 func TestExample(t *testing.T) {
   // connect
   log.Println("start test")
-  s,err := Connect("http://pampa.dev:8529","diego","test",true)
+  s,err := Connect("http://pampa.dev:8529","diego","test",false)
   if err != nil{
     panic(err)
   }
-  db := s.DB("_system")
+  db := s.DB("pampa")
+
+  col := db.Col("users")  
+  uniq ,_:= col.Unique("Username","probando",true,"")
+
+  log.Println(uniq)
+
+  var test DocTest
+  var e Error
+  e = make(map[string]string)
+
+  Unique(test,db,true,e)
 
   if db == nil {
     panic("invalid db")
   }
-  // test relations
-  //ed := NewEdgeDefinition("edges",[]string{ "test" }, []string{ "test" })
-  ed2 := NewEdgeDefinition("runs",[]string{ "test" }, []string{ "campaigns" })
-  db.Graph("test1").AddEdge(ed2)
+
+  var q AqlStruct
+
+  type F struct {
+    Filters []Filters `json:"filters"`
+  }
+  var f F
+
+  post := `{ "filters" : [  {"conditions" : [ { "obj": "ops" , "op":"==" , "val":"1" },{ "obj": "type" , "op":"==" , "val":"CPM" }]}]    }`
+
+  err = json.Unmarshal([]byte(post),&f)
+  if err != nil {
+    panic(err)
+  }
+  log.Println(f)
+  q.For("acc").In("accounts")
+  q.Filter("acc",f.Filters[0].Filter)
+//q.Return(map[string]interface{}{ "acc" : "acc"})
+  q.Custom("RETURN acc")
+
+
+  w :=  []string{ "Pot","Jon","snow"}
+  in := []string{ "name", "surname" }
+  c := "test1"
+
+  text := FullText(w,in,c)
+  log.Println(text)
 
 }
