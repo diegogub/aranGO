716b6fd7840d783c2e6eecf5f41463adcdcc3d0a
diego <diegogub@gmail.com>
2016-11-11 13:28:38 +0400

    fix collection list

--- collection.go
+++ collection.go
@@ -333,7 +333,7 @@ func Collections(db *Database) error {
 	var res *response
 
 	var cols struct {
-		Result []Collection `json:"collections"`
+		Result []Collection `json:"result"`
 	}
 	// get all non-system collections
 	res, err = db.get("collection?excludeSystem=true", "", "GET", nil, &cols, db)
